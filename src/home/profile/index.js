5ee75e10c30a9cd9af200e20855607cd30447b3a
prathana-kachchhi <163413657+prathana-kachchhi@users.noreply.github.com>
2024-03-14 15:44:38 +0000

    some changes...

---
+++ src/home/profile/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './profile.scss';
+
+export default function Profile() {
+    return (
+        <div className='profile'>
+            <div className="container">
+                <section>
+                    <div className="heading">
+                        <p>Hello I'm</p>
+                        <h1>Prathana kachchhi</h1>
+                        <span>Full Stack Developer</span>
+                    </div>
+                </section>
+            </div>
+        </div>
+    )
+}
